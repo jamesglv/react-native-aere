17b8e3fa06542c0f37e2f0aff9f567e994ea911d
jamesglv <jamesogilvie@live.com>
2024-11-03 09:48:13 +1000

    Filter Modal UI

--- components/FilterModal.jsx
+++ components/FilterModal.jsx
@@ -1,11 +1,12 @@
-// FilterModal.jsx
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Modal, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, Alert, TouchableWithoutFeedback } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import RangeSlider from 'react-native-range-slider-expo';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import Slider from '@react-native-community/slider';
+import { LinearGradient } from 'expo-linear-gradient';
 import { getAuth } from 'firebase/auth';
-import { updateUserDocument } from '../firebaseActions'; // Import updateUserDocument
+import { updateUserDocument } from '../firebaseActions';
 
 const FIREBASE_AUTH = getAuth();
 
@@ -29,10 +30,8 @@ const FilterModal = ({
     
     setSelectedGenders(updatedGenders);
 
-    // Ensure the user is authenticated and matches the current ID
     const user = FIREBASE_AUTH.currentUser;
     if (user && user.uid === currentUserId) {
-      // Update gender preferences in the Firestore database
       try {
         await updateUserDocument({ interested: updatedGenders });
         console.log("User's interested field updated successfully");
@@ -53,61 +52,82 @@ const FilterModal = ({
       visible={filterModalVisible}
       onRequestClose={toggleFilterModal}
     >
-      <View style={styles.modalOverlay}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Filter by Age</Text>
-          <RangeSlider
-            min={18}
-            max={60}
-            fromValueOnChange={setMinAge}
-            toValueOnChange={setMaxAge}
-            initialFromValue={minAge}
-            initialToValue={maxAge}
-            styleSize="medium"
-          />
-          <View style={styles.ageLabelContainer}>
-            <Text style={styles.ageLabel}>Min Age: {minAge}</Text>
-            <Text style={styles.ageLabel}>Max Age: {maxAge}</Text>
-          </View>
+      <TouchableOpacity style={styles.modalOverlay} activeOpacity={1} onPress={toggleFilterModal}>
+        <LinearGradient
+          colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.9)', 'rgba(0, 0, 0, 0.7)']}
+          style={styles.gradientOverlay}
+        >
+          <TouchableWithoutFeedback>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle} className='font-oregular'>Age</Text>
+              <MultiSlider
+                values={[minAge, maxAge]}
+                onValuesChange={(values) => {
+                  setMinAge(values[0]);
+                  setMaxAge(values[1]);
+                }}
+                min={18}
+                max={60}
+                step={1}
+                selectedStyle={{ backgroundColor: 'black' }}
+                unselectedStyle={{ backgroundColor: '#ddd' }}
+                sliderLength={350}
+                markerStyle={{
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  height: 30,
+                  width: 30,
+                }}
+                containerStyle={{
+                  height: 50,
+                  width: '90%',
+                }}
+                trackStyle={{
+                  height: 4,
+                }}
+              />
 
-          <Text style={styles.modalTitle}>Filter by Distance</Text>
-          <Slider
-            style={{ width: '90%', height: 60 }}
-            minimumValue={1}
-            maximumValue={500}
-            value={maxDistance}
-            onValueChange={setMaxDistance}
-            minimumTrackTintColor="#007bff"
-            maximumTrackTintColor="#ddd"
-            thumbTintColor="#007bff"
-          />
-          <View style={styles.distanceLabelContainer}>
-            <Text style={styles.distanceLabel}>Max Distance: {Math.round(maxDistance)} km</Text>
-          </View>
+              <View style={styles.ageLabelContainer}>
+                <Text style={styles.ageLabel} className='font-oregular'>{minAge}</Text>
+                <Text style={styles.ageLabel} className='font-oregular'>{maxAge}</Text>
+              </View>
 
-          <Text style={styles.modalTitle}>Filter by Gender</Text>
-          <View style={styles.checkboxContainer}>
-            {['Male', 'Female', 'Non-Binary'].map((gender) => (
-              <TouchableOpacity
-                key={gender}
-                style={styles.checkbox}
-                onPress={() => toggleGender(gender)}
-              >
-                <Ionicons
-                  name={selectedGenders.includes(gender) ? 'checkbox' : 'square-outline'}
-                  size={24}
-                  color={selectedGenders.includes(gender) ? '#007bff' : '#ddd'}
-                />
-                <Text style={styles.checkboxLabel}>{gender}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
+              <Text style={styles.modalTitle} className='font-oregular'>Distance</Text>
+              <Slider
+                style={{ width: '90%', height: 60 }}
+                minimumValue={1}
+                maximumValue={500}
+                value={maxDistance}
+                onValueChange={setMaxDistance}
+                minimumTrackTintColor="black"
+                maximumTrackTintColor="#ddd"
+                thumbTintColor="black"
+              />
+              <View style={styles.distanceLabelContainer}>
+                <Text style={styles.distanceLabel} className='font-oregular'>Max Distance: {Math.round(maxDistance)} km</Text>
+              </View>
 
-          <TouchableOpacity onPress={toggleFilterModal} style={styles.closeModalButton}>
-            <Text style={styles.closeModalText}>Close</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+              <Text style={styles.modalTitle} className='font-oregular'>Gender</Text>
+              <View style={styles.checkboxContainer}>
+                {['Male', 'Female', 'Non-Binary'].map((gender) => (
+                  <TouchableOpacity
+                    key={gender}
+                    style={styles.checkbox}
+                    onPress={() => toggleGender(gender)}
+                  >
+                    <Ionicons
+                      name={selectedGenders.includes(gender) ? 'checkmark-circle' : 'ellipse-outline'}
+                      size={24}
+                      color={selectedGenders.includes(gender) ? 'black' : '#ddd'}
+                    />
+                    <Text style={styles.checkboxLabel} className='font-oregular'>{gender}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </LinearGradient>
+      </TouchableOpacity>
     </Modal>
   );
 };
@@ -115,6 +135,11 @@ const FilterModal = ({
 // Styles specific to FilterModal
 const styles = StyleSheet.create({
   modalOverlay: { flex: 1, justifyContent: 'flex-end' },
+  gradientOverlay: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'flex-end',
+  },
   modalContent: {
     width: '100%',
     height: '70%',
@@ -124,26 +149,36 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 30,
     alignItems: 'center',
   },
-  modalTitle: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  modalTitle: { fontSize: 20, fontWeight: 'bold', marginBottom: 20, marginTop: 10 },
   ageLabelContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
     paddingHorizontal: 20,
-    marginVertical: 10,
+    marginTop: 10,
+    marginBottom: 30,
   },
-  ageLabel: { fontSize: 16, fontWeight: 'bold' },
-  distanceLabelContainer: { alignItems: 'center', marginVertical: 10 },
+  ageLabel: { fontSize: 20 },
+  distanceLabelContainer: { alignItems: 'center', marginTop: 15, marginBottom: 50, },
   distanceLabel: { fontSize: 16, fontWeight: 'bold' },
   checkboxContainer: {
     flexDirection: 'column',
     alignItems: 'flex-start',
     marginBottom: 20,
+    width: '100%',
   },
-  checkbox: { flexDirection: 'row', alignItems: 'center', marginBottom: 10 },
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderColor: '#f0f0f0',
+    borderWidth: 2,
+    borderRadius: 25,
+    marginBottom: 8,
+  },
   checkboxLabel: { marginLeft: 10, fontSize: 18, color: '#333' },
-  closeModalButton: { marginTop: 20 },
-  closeModalText: { color: '#007bff', fontSize: 18 },
 });
 
 export default FilterModal;
